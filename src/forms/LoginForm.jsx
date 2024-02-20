77481a18b317ff9f6637db7b803ae165de7dd6cc
shaheen-uddin <shaheenelt@gmail.com>
2024-02-20 16:13:44 +0600

    Validation introduced.

--- src/forms/LoginForm.jsx
+++ src/forms/LoginForm.jsx
@@ -1,15 +1,21 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 import FieldSet from "../components/FieldSet";
 import Field from "../components/Field";
 
 export default function LoginForm() {
+  const { register, handleSubmit } = useForm();
+  const submitForm = (formData) => {
+    console.log(formData);
+  };
   return (
-    <div>
-      <form>
+    <div className="flex flex-col justify-center items-center">
+      <form onSubmit={handleSubmit(submitForm)}>
         <FieldSet label="Enter Login Label">
           <Field label="Email">
             <input
+              {...register("email", { required: "Email is required." })}
               className="p-1 border box-border w-[300px] rounded-md border-gray-200"
               type="email"
               name="email"
@@ -18,6 +24,13 @@ export default function LoginForm() {
           </Field>
           <Field label="Password">
             <input
+              {...register("password", {
+                required: "Password is required",
+                minLength: {
+                  value: 8,
+                  message: "Your password must be at least 8 characters.",
+                },
+              })}
               className="p-1 border box-border w-[300px] rounded-md border-gray-200"
               type="password"
               name="password"
